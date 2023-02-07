661aec4549d9d1bc88635e95bc3a210198022150
Ethan Ford <ethan.ford06@gmail.com>
2023-02-06 21:15:58 -0600

    ahhhh

---
+++ src/kairos.rs
@@ -0,0 +1,7 @@
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{group, hook};
+use serenity::framework::standard::StandardFramework;
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use dotenv::dotenv;
+use std::env::var;
+
+mod kairos;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let bot_token = var("DISCORD_TOKEN").expect("Discord Token");
+
 }
